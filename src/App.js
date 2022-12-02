627c596fb8958792cccad6efee0296adba30a0ec
SharmilaNarendra <sharmilanbabu@gmail.com>
2022-12-02 23:36:16 +0530

    creating create-react-app with parcel

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+    
+    import React from 'react';
+    import ReactDOM from 'react-dom/client';
+     
     //Javascript 
     const jsRoot = document.getElementById("js-root");
     const jsHeading = document.createElement("h1");
@@ -7,6 +11,6 @@
     
     
     //React 
-    const reactRoot = ReactDOM.createRoot(document.getElementById("js-root"));
+    const reactRoot = ReactDOM.createRoot(document.getElementById("react-root"));
     const raectHeading = React.createElement("h1",{},"Hello React!!!");
     reactRoot.render(raectHeading);
