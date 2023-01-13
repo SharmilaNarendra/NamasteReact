4fb983d69fc7036d0dbbf4f8841efb2ebc4ccb22
Sharmila Narendra Babu <86154019+SharmilaNarendra@users.noreply.github.com>
2023-01-13 21:51:57 +0530

    feat: implementing lazy loading

--- src/components/error/ErrorCommponent.js
+++ src/components/error/ErrorCommponent.js
@@ -4,11 +4,13 @@ const ErrorComponent = () => {
   const errorData = useRouteError();
 
   return (
-    <div className="no-results">Somthing went wrong !
-    <p>{errorData.statusText || errorData.mesage}</p>
-    </div>
-
-  )
+    <>
+      <div className="no-results">Somthing went wrong !</div>
+      <p class="no-results">
+        {errorData.statusText ? errorData.statusText : errorData.message}
+      </p>
+    </>
+  );
 };
 
 export default ErrorComponent;
--- src/router/router.js
+++ src/router/router.js
@@ -1,27 +1,36 @@
 import { createBrowserRouter } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
 
-import AppLayout from "../components/AppLayout"
+import AppLayout from "../components/AppLayout";
 import ErrorComponent from "../components/error/ErrorCommponent";
 import SearchComponent from "../components/search/SearchComponent";
-import MemberDetail from "../components/memberDetail/MemberDetail";
+//import MemberDetail from "../components/memberDetail/MemberDetail";
+
+const MemberDetail = lazy(() =>
+  import("../components/memberDetail/MemberDetail")
+);
 
 const appRouter = createBrowserRouter([
-    { 
-        path: "/",
-        element: <AppLayout />,
-        errorElement:<ErrorComponent/>,
-        children:[
-            { index: true, element: <SearchComponent /> },
-            {
-                path:"/search",
-                element:<SearchComponent />
-            },
-            {
-                path:"/teamMember/:id",
-                element:<MemberDetail />
-            }
-        ]
-    }
-]); 
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorComponent />,
+    children: [
+      { index: true, element: <SearchComponent /> },
+      {
+        path: "/search",
+        element: <SearchComponent />,
+      },
+      {
+        path: "/teamMember/:id",
+        element: (
+          <Suspense fallback={<h1>Loading ...</h1>}>
+            <MemberDetail />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
 
 export default appRouter;
