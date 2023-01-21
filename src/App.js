0389d398b3b94e30d5544dbf533a30c97763aece
Sharmila Narendra Babu <86154019+SharmilaNarendra@users.noreply.github.com>
2023-01-21 16:57:38 +0530

    feat: Added dark mode light mode - useContext

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import ReactDOM from "react-dom/client";
-import {RouterProvider} from "react-router-dom"
+import { RouterProvider } from "react-router-dom";
 
-import appRouter from "./router/router"
+import appRouter from "./router/router";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'react-loading-skeleton/dist/skeleton.css'
+import "react-loading-skeleton/dist/skeleton.css";
 
 /** accessing DOM to render react element */
 const reactRoot = ReactDOM.createRoot(document.getElementById("react-root"));
-reactRoot.render(<RouterProvider router={appRouter}/>);
-
+reactRoot.render(<RouterProvider router={appRouter} />);
